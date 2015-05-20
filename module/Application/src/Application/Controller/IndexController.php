b6fcf9ca904b016ab155b77ee43fc4fb4d843902
Paulo Cella <paulocella@unochapeco.edu.br>
2015-05-19 22:03:02 -0300

    saasdlasdk' '

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+**
+ *  @author Paulo Cella <paulocella@unochapeco.edu.br>
+ * 
+ * 
  */
 
 namespace Application\Controller;
@@ -16,7 +15,15 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        $post = $em->getRepository('\Main\Entity\Post')->findAll();
+        
+    return new ViewModel(
+                array(
+            'post' => $post
+                )
+        );
+        
     }
 	
     public function saveAction()
