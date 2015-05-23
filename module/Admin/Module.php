e0ad5eb7608f703d6d355426a271a629c789f2b0
Paulo Cella <paulocella@unochapeco.edu.br>
2015-05-23 00:50:01 -0300

    skeleton

--- module/Admin/Module.php
+++ module/Admin/Module.php
@@ -1,15 +1,34 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
 
-namespace Admin;
+namespace Main;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
 
 class Module
 {
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
@@ -17,63 +36,4 @@ class Module
             ),
         );
     }
-
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-    
-    public function getServiceConfig(){
-        
-        return array(
-            'factories' => array(
-                'Zend\Authentication\AuthenticationService' => function($serviceManager){
-                    return $serviceManager->get('doctrine.authenticationservice.orm_default');
-                }
-            )
-        );
-    }
-    
-    /**
-     * Executa no bootstrap do módulo
-     * 
-     * @param MvcEvent $e
-     */
-    public function onBootstrap($e){
-        /** @var \Zend\ModuleManager\ModuleManager $moduleManager */
-        $moduleManager = $e->getApplication()->getServiceManager()->get('modulemanager');
-        /** @var \Zend\ModuleManager\ModuleManager $sharedEvents */
-        $sharedEvents = $moduleManager->getEventManager()->getSharedManager();
-        
-        //adicionaeventos ao modulo
-        $sharedEvents->attach('Zend\Mvc\Controller\AbstractActionController', \Zend\Mvc\MvcEvent::EVENT_DISPATCH,array($this, 'mvcPreDispatch'),100);
-        
-    }
-    
-    /**
-     * Verifica se precisa fazer a autorizacao do acesso 
-     * @param MvcEvent $event Evento
-     * @return boolean
-     */
-    public function mvcPreDispatch($event){
-        
-        $di = $event->getTarget()->getServiceLocator();
-        $routeMatch = $event->getRouteMatch();
-        $moduleName = $routeMatch->getParam('module');
-        $controllerName = $routeMatch->getParam('controller');
-        $actionName = $routeMatch->getParam('action');
-        $authService = $di->get('Admin\Service\Auth');
-        
-        
-        
-        if(!$authService->authorize($moduleName,$controllerName,$actionName)){
-            throw new \Exception('Voce não tem permissao para acessar esse recurso!');
-        }
-        
-        
-        return true;
-        
-    }
-        
-   }
-    
+}
--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -1,113 +1,102 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonAdmin for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
 
-// module/Admin/conﬁg/module.config.php:
 return array(
-    'controllers' => array(//add module controllers
-        'invokables' => array(
-            'Admin\Controller\Index' => 'Admin\Controller\IndexController',
-            
-        ),
-    ),
-//Configuração doctrine
-    'doctrine' => array(
-        'driver' => array(
-            'application_entities' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => array(__DIR__ . '/../src/Admin/Entity')
-            ),
-            'orm_default' => array(
-                'drivers' => array(
-                    'Admin\Entity' => 'application_entities'
-                )
-            ))),
-//*********************************************************
     'router' => array(
         'routes' => array(
-            'admin' => array(
-                'type' => 'Literal',
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
-                    'route' => '/admin',
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'Admin\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            // The following is a route to simplify getting started creating
+            // new controllers and actions without needing to create a new
+            // module. Simply drop new controllers in, and you can access them
+            // using the path /admin/:controller/:action
+            'admin' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/admin',
                     'defaults' => array(
                         '__NAMESPACE__' => 'Admin\Controller',
-                        'controller' => 'Index',
-                        'action' => 'index',
-                        'module' => 'admin'
+                        'controller'    => 'Index',
+                        'action'        => 'index',
                     ),
                 ),
                 'may_terminate' => true,
                 'child_routes' => array(
                     'default' => array(
-                        'type' => 'Segment',
+                        'type'    => 'Segment',
                         'options' => array(
-                            'route' => '/[:controller[/:action]]',
+                            'route'    => '/[:controller[/:action]]',
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
                             ),
                             'defaults' => array(
                             ),
                         ),
-                        'child_routes' => array(//permite mandar dados pela url
-                            'wildcard' => array(
-                                'type' => 'Wildcard'
-                            ),
-                        ),
                     ),
                 ),
             ),
-            'usuarios' => array(
-                'type' => 'segment',
-                'options' => array(
-                    'route' => '/admin/usuarios/index/[page/:page]',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Admin\Controller',
-                        'controller' => 'Usuarios',
-                        'action' => 'index',
-                        'module' => 'admin',
-                        'page' => 1,
-                    ),
-                ),
-            ),
-            
-            'interesses' => array(
-                'type' => 'segment',
-                'options' => array(
-                    'route' => '/admin/interesses/index/[page/:page]',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Admin\Controller',
-                        'controller' => 'Interesses',
-                        'action' => 'index',
-                        'module' => 'admin',
-                        'page' => 1,
-                    ),
-                ),
-            ),
-            
         ),
     ),
-    
     'service_manager' => array(
-        'factories' => array(
-            'Session' => function ($sm){
-                return new Zend\Session\Container('SessionAdmin');
-            },
-        )
-    ),
-    
-    'view_manager' => array(//the module can have a specific layout
-// 'template_map' => array(
-// 'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-// ),
-        'template_path_stack' => array(
-            'admin' => __DIR__ . '/../view',
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
+        'aliases' => array(
+            'translator' => 'MvcTranslator',
         ),
     ),
-        /* 'db' => array( //module can have a specific db configuration
-          'driver' => 'PDO_SQLite',
-          'dsn' => 'sqlite:' . __DIR__ .'/../data/admin.db',
-          'driver_options' => array(
-          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-          )
-          ) */
+    'translator' => array(
+        'locale' => 'en_US',
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Admin\Controller\Index' => 'Admin\Controller\IndexController'
+        ),
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'admin/index/index' => __DIR__ . '/../view/admin/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    // Placeholder for console routes
+    'console' => array(
+        'router' => array(
+            'routes' => array(
+            ),
+        ),
+    ),
 );
